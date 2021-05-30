3feb953d127d0e425a3a74e404b18a178ff79345
lan-mak <lan-mak@ya.ru>
2021-05-30 23:57:06 +0300

    ver 0.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,48 @@
+'use strict';
+
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
-  return arr; // array
+  arr = []
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant < 0) {
+    arr
+  } else if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a))
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a))
+    console.log(arr)
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
+  let data = {
+    percentData: percent,
+    contributionData: contribution,
+    amountData: amount,
+    dateData: date,
+  }
 
+  for (let item in data) {
+    if (typeof data[item] === 'string') {
+      data[item] = +data[item]
+    }
+    if (isNaN(data[item])) {
+      console.log(`Параметр ${item} содержит неправильное значение ${percent}`)
+    }
+  }
+
+  const totalDebt = data.amountData - data.contributionData;
+  const creditTerm = ((date.getTime() - Date.now()) / 2629800000).toFixed(0)
+  const monthlyPercent = (data.percentData / 100) / 12;
+  const monthlyPayment = totalDebt * (monthlyPercent + monthlyPercent / ((Math.pow((1 + monthlyPercent), creditTerm)) - 1))
+  totalAmount = +((monthlyPayment * creditTerm).toFixed(2))
+  
+  console.log(`ответ: ${totalAmount}`)
   return totalAmount;
 }
