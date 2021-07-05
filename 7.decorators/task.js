c26b43274dcb0a4e7f366ef6a5bb7b641e9fabf9
lan-mak <lan-mak@ya.ru>
2021-07-05 07:58:11 +0300

    v.0.01a

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,15 +1,82 @@
-function compareArrays(arr1, arr2) {
-  let result;
+// Задача №1
 
+function cachingDecoratorNew(func) {
   // Ваш код
+  let cache = [];
 
-  return result; // boolean
+  function wrapper(...args) {
+
+    let checkCache = cache.findIndex(item => item.args === args.join(','));
+
+    if (checkCache >= 0) {
+      console.log(cache[checkCache].count)
+      return "Из кэша: " + cache[checkCache].count;
+    } else if (checkCache < 0) {
+      return checkArrLength()
+    }
+
+    function checkArrLength() {
+      let objItem = {
+        args: args.join(','),
+        count: args.reduce((sum, current) => sum + current)
+      };
+
+      if (cache.length < 5) {
+        cache.push(objItem);
+        return "Вычисляем: " + objItem.count;
+      } else if (cache.length >= 5) {
+        cache.shift();
+        cache.push(objItem);
+        return "Вычисляем: " + objItem.count;
+      }
+    }
+  }
+
+  return wrapper
 }
 
-function advancedFilter(arr) {
-  let resultArr;
 
+const addThree = (a, b, c) => a + b + c;
+const upgradedAddThree = cachingDecoratorNew(addThree);
+upgradedAddThree(1, 2, 3); // вычисляем: 6
+upgradedAddThree(1, 2, 3); // из кэша: 6
+upgradedAddThree(2, 2, 3); // вычисляем: 7
+upgradedAddThree(3, 2, 3); // вычисляем: 8
+upgradedAddThree(4, 2, 3); // вычисляем: 9
+upgradedAddThree(5, 2, 3); // вычисляем: 10
+
+
+// Задача №2
+
+function debounceDecoratorNew(f, ms) {
   // Ваш код
+  let timeout;
 
-  return resultArr; // array
+  function wrapper(...args) {
+    if (timeout === undefined) {
+      f()
+    }
+
+
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      f.apply(this, args);
+    }, ms);
+  };
+
+  return wrapper;
+}
+
+const sendSignal = () => console.log("Сигнал отправлен");
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+setTimeout(upgradedSendSignal); // Сигнал отправлен
+setTimeout(upgradedSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс
+setTimeout(upgradedSendSignal, 900); // проигнорировано аналогично
+setTimeout(upgradedSendSignal, 1200); // проигнорировано аналогично
+setTimeout(upgradedSendSignal, 2300); // проигнорировано аналогично
+setTimeout(upgradedSendSignal, 4400); // Сигнал отправлен
+setTimeout(upgradedSendSignal, 4500); // проигнорировано аналогично
+
+function debounceDecorator2(func) {
+  // Ваш код
 }
