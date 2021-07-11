4691eb6386ff9253814029f2645d662420736c6b
lan-mak <lan-mak@ya.ru>
2021-07-11 16:28:46 +0300

    ver 0.09a

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,76 +1,75 @@
 // Задача №1
 
-// function cachingDecoratorNew(func) {
-//   // Ваш код
-//   let cache = [];
+function cachingDecoratorNew(func) {
+  // Ваш код
+  let cache = [];
 
-//   function wrapper(...args) {
-
-//     let checkCache = cache.findIndex(item => item.args === args.join(','));
-
-
-//     if (checkCache >= 0) {
-//       // console.log(cache[checkCache].count)
-//       return "Из кэша: " + cache[checkCache].count;
-//     } else if (checkCache < 0) {
-//       return checkArrLength()
-//     }
-
-//     function checkArrLength() {
-//       let objItem = {
-//         args: args.join(','),
-//         count: args.reduce((sum, current) => sum + current)
-//       };
-
-//       if (cache.length < 5) {
-//         cache.push(objItem);
-//         return "Вычисляем: " + objItem.count;
-//       } else if (cache.length >= 5) {
-//         cache.shift();
-//         cache.push(objItem);
-//         return "Вычисляем: " + objItem.count;
-//       }
-//     }
-//   }
-
-//   return wrapper
-// }
+  function wrapper(...args) {
+  
+    let checkCache = cache.findIndex(item => item.args === args.join(','));
 
 
-// const addThree = (a, b, c) => a + b + c;
-// const upgradedAddThree = cachingDecoratorNew(addThree);
-// upgradedAddThree(1, 2, 3); // вычисляем: 6
-// upgradedAddThree(1, 2, 3); // из кэша: 6
-// upgradedAddThree(2, 2, 3); // вычисляем: 7
-// upgradedAddThree(3, 2, 3); // вычисляем: 8
-// upgradedAddThree(4, 2, 3); // вычисляем: 9
-// upgradedAddThree(5, 2, 3); // вычисляем: 10
+    if (checkCache >= 0) {
+      return "Из кэша: " + cache[checkCache].count;
+    } else if (checkCache < 0) {
+      return checkArrLength()
+    }
+
+    function checkArrLength() {
+      let objItem = {
+        args: args.join(','),
+        count: func(...args)
+      };
+
+      if (cache.length < 5) {
+        cache.push(objItem);
+        return "Вычисляем: " + objItem.count;
+      } else if (cache.length >= 5) {
+        cache.shift();
+        cache.push(objItem);
+        return "Вычисляем: " + objItem.count;
+      }
+    }
+  }
+
+  return wrapper
+}
+
+
+const addThree = (a, b, c) => a + b + c;
+const upgradedAddThree = cachingDecoratorNew(addThree);
+upgradedAddThree(1, 2, 3); // вычисляем: 6
+upgradedAddThree(1, 2, 3); // из кэша: 6
+upgradedAddThree(2, 2, 3); // вычисляем: 7
+upgradedAddThree(3, 2, 3); // вычисляем: 8
+upgradedAddThree(4, 2, 3); // вычисляем: 9
+upgradedAddThree(5, 2, 3); // вычисляем: 10
 
 
 // Задача №2
 
-// function debounceDecoratorNew(f, ms) {
-//   // Ваш код
-//   let timer;
-//   let flag = false
+function debounceDecoratorNew(f, ms) {
+  // Ваш код
+  let timer;
+  let flag = false
 
-//   function wrapper(...args) {
-//     if (!flag) {
-//       f()
-//       flag = true;
-//       timer = setTimeout(() => {
-//         flag = false
-//       }, ms)
-//     } else {
-//       clearTimeout(timer)
-//       timer = setTimeout(() => {
-//         flag = false
-//       }, ms)
-//     }
-//   }
+  function wrapper(...args) {
+    if (!flag) {
+      f()
+      flag = true;
+      timer = setTimeout(() => {
+        flag = false
+      }, ms)
+    } else {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        flag = false
+      }, ms)
+    }
+  }
 
-//   return wrapper;
-// }
+  return wrapper;
+}
 
 
 // const sendSignal = () => console.log("Сигнал отправлен");
@@ -87,16 +86,22 @@
 // Задача №3
 
 function debounceDecorator2(func, ms) {
+  // Ваш код
+  let timer;
   let flag = false
   wrapper.count = null;
 
   function wrapper(...args) {
     wrapper.count += 1;
-    console.log('Количество вызовов: ', wrapper.count)
     if (!flag) {
       func()
       flag = true;
-      setTimeout(() => {
+      timer = setTimeout(() => {
+        flag = false
+      }, ms)
+    } else {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
         flag = false
       }, ms)
     }
@@ -105,12 +110,13 @@ function debounceDecorator2(func, ms) {
   return wrapper;
  }
 
-const sendSignal = () => console.log("Сигнал отправлен");
-const upgSendSignal = debounceDecorator2(sendSignal, 2000);
-setTimeout(upgSendSignal); // Сигнал отправлен
-setTimeout(upgSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс
-setTimeout(upgSendSignal, 900); // проигнорировано аналогично
-setTimeout(upgSendSignal, 1200); // проигнорировано аналогично
-setTimeout(upgSendSignal, 2300); // проигнорировано аналогично
-setTimeout(upgSendSignal, 4400); // Сигнал отправлен
-setTimeout(upgSendSignal, 4500); // проигнорировано аналогично
+// const sendSignal = () => console.log("Сигнал отправлен");
+// const upgSendSignal = debounceDecorator2(sendSignal, 2000);
+// setTimeout(upgSendSignal); // Сигнал отправлен
+// setTimeout(upgSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс
+// setTimeout(upgSendSignal, 900); // проигнорировано аналогично
+// setTimeout(upgSendSignal, 1200); // проигнорировано аналогично
+// setTimeout(upgSendSignal, 2300); // проигнорировано аналогично
+// setTimeout(upgSendSignal, 4400); // Сигнал отправлен
+// setTimeout(upgSendSignal, 4500); // проигнорировано аналогично
+// setTimeout(() => console.log(upgSendSignal.count), 5000)
