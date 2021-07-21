1a19ece861b583a1a90318c031e1ec4bea51a73b
lan-mak <lan-mak@ya.ru>
2021-07-21 20:31:20 +0300

    ver 0.01a

--- 8.async/task.js
+++ 8.async/task.js
@@ -1,37 +1,89 @@
 "use strict";
 
 class AlarmClock {
-  // методы класса
   constructor() {
     this.alarmCollection = [];
-    this.timerId = [1, 2, 3];
+    this.timerId = null;
   }
-  
-  addClock(doTimer = '20,30', cellback = 'какая-то функция', id = 2) {
-    let timer = new Date();
-    let mDoTimer = doTimer.split(',')
-    timer.setHours(mDoTimer[0], mDoTimer[1]);
-    console.log(`${timer.getHours()}:${timer.getMinutes()}`)
-    
-    let mCellback = cellback;
-    console.log(mCellback)
+
+  addClock(doTimer, cellback, id) {
+    let itemCell = {};
+
+    itemCell.timeAction = doTimer
+    itemCell.func = cellback;
+    itemCell.id = id;
 
     if (!id) {
       throw new Error('Параметр id не передан')
     }
 
-    console.log(id)
-    this.timerId.forEach(function(item) {
-      if (item === id) {
-        return true;
-      } 
-    })
-    
+    let addCheckId;
 
+    this.alarmCollection.forEach(element => {
+      if (element.id === id) {
+        console.error('такой id существует')
+        return addCheckId = true;
+      }
+    });
+
+    if (!addCheckId) {
+      this.alarmCollection.push(itemCell)
+    }
+  }
+
+  removeClock(id) {
+    let removeCheckId = this.alarmCollection.findIndex(item => {
+      return item.id === id
+    })
+
+    if (removeCheckId >= 0) {
+      this.alarmCollection.splice(removeCheckId, 1)
+      console.warn('id удален = true')
+      return true
+    } else {
+      console.warn('id удален = true')
+      return false
+    }
+  }
+
+  getCurrentFormattedTime() {
+
+    let Data = new Date();
+    let Hour = Data.getHours() < 10 ? `0${Data.getHours()}` : `${Data.getHours()}`;;
+    let Minutes = Data.getMinutes() < 10 ? `0${Data.getMinutes()}` : `${Data.getMinutes()}`; ;
+
+    // console.log(Hour + ":" + Minutes, 'текущее время')
+    return `${Hour}:${Minutes}`
+  }
+
+  start() {
+
+    let currentTime = this.getCurrentFormattedTime()
+    this.alarmCollection.forEach(checkClock)
+
+    console.log(currentTime)
+
+    function checkClock(item) {
+      if (item.timeAction === currentTime) {
+        item.func()
+      } 
+    }
+
+    if (this.timerId === null) {
+      this.timerId = setInterval(() => {
+        this.alarmCollection.forEach(checkClock)
+      });
+    }
   }
 }
 
 
 
 const test = new AlarmClock;
-test.addClock()
+test.addClock('20:20', f => f, 5)
+test.addClock('20:25', f => f, 1)
+test.addClock('20:25', f => f, 5)
+test.addClock('23:22', f => f, 6)
+
+test.removeClock(7)
+test.start()
